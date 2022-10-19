f7af511eb9cce757629065d772cd08f6c2025a1f
RagnarLodbrok2017 <35165213+RagnarLodbrok2017@users.noreply.github.com>
2022-10-19 04:02:10 +0200

    Finished Login Register Forget
    and Dashboard

---
+++ resources/js/routes.js
@@ -0,0 +1,32 @@
+let dashboardComponent =  require('./components/dashboard/DashboardComponent.vue').default;
+let loginComponent =  require('./components/auth/LoginComponent.vue').default;
+let registerComponent =  require('./components/auth/RegisterComponent.vue').default;
+let forgetPasswordComponent =  require('./components/auth/ForgetPasswordComponent.vue').default;
+
+
+
+
+
+
+export const routes = [
+    {
+        path:'/login',
+        name : '/login',
+        component:loginComponent
+    },
+    {
+        path:'/register',
+        name : '/register',
+        component:registerComponent
+    },
+    {
+        path:'/forget',
+        name : '/forget',
+        component:forgetPasswordComponent
+    },
+    {
+        path:'/',
+        name : '/',
+        component:dashboardComponent
+    }
+]
